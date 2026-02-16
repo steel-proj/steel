7f674168847ca342d07053ca809ec302e8701961
Max Tasker <mmttasker@gmail.com>
2026-02-16 12:44:41 +0000

    switched fully to using s_assert rather than std::runtime_exception

--- steelc/src/mir/builders/mir_builder.cpp
+++ steelc/src/mir/builders/mir_builder.cpp
@@ -1,7 +1,5 @@
 #include "mir_builder.h"
 
-#include <stdexcept>
-
 #include <representations/types/data_type.h>
 #include <representations/types/function_type.h>
 #include <mir/mir_block.h>
@@ -9,6 +7,7 @@
 #include <mir/mir_value.h>
 #include <mir/mir_instr.h>
 #include <mir/mir_operand.h>
+#include <utils/assert.h>
 
 void mir_builder::build_ret_void() {
 	insert_instr({
@@ -42,21 +41,50 @@ mir_value mir_builder::build_mod(mir_operand lhs, mir_operand rhs, const std::st
 	return build_binary_op(mir_instr_opcode::MOD, lhs, rhs, result_name);
 }
 mir_value mir_builder::build_binary_op(mir_instr_opcode opcode, mir_operand lhs, mir_operand rhs, const std::string& result_name) {
-	if (!check_type_match(lhs, rhs)) {
-		throw std::runtime_error("Type mismatch in binary operation instruction");
+	s_assert(check_type_match(lhs, rhs),
+		"Types don't match in binary operation");
+
+	mir_type result_type = mir_type{nullptr};
+	switch (opcode) {
+	// arithmetic - use lhs
+	case mir_instr_opcode::ADD:
+	case mir_instr_opcode::SUB:
+	case mir_instr_opcode::MUL:
+	case mir_instr_opcode::DIV:
+	case mir_instr_opcode::MOD:
+		result_type = operand_type(lhs);
+		break;
+	// comparison - always bool
+	case mir_instr_opcode::CMP_EQ:
+	case mir_instr_opcode::CMP_NEQ:
+	case mir_instr_opcode::CMP_LT:
+	case mir_instr_opcode::CMP_LTE:
+	case mir_instr_opcode::CMP_GT:
+	case mir_instr_opcode::CMP_GTE:
+		result_type = mir_type{ data_type::get(DT_BOOL) };
+		break;
+	// logical - always bool
+	case mir_instr_opcode::AND:
+	case mir_instr_opcode::OR:
+	case mir_instr_opcode::NOT:
+		result_type = mir_type{ data_type::get(DT_BOOL) };
+		break;
 	}
 
+	s_assert(result_type.ty != nullptr,
+		"Failed to identify binary operation result type");
+
 	// both same - just use lhs
-	mir_value result = create_ssa_value(operand_type(lhs), result_name);
+	mir_value result = create_ssa_value(result_type, result_name);
 	insert_instr({
 		.kind = opcode,
-		.type = operand_type(lhs),
+		.type = result_type,
 		.result = result,
 		.operands = {
 			mir_operand{lhs},
 			mir_operand{rhs}
 		}
-		});
+	});
 	return result;
 }
 
@@ -86,22 +114,30 @@ void mir_builder::build_branch(mir_block* target) {
 	insert_instr({
 		.kind = mir_instr_opcode::BRA,
 		.operands = {
-			mir_block_ref{target}
+			mir_block_ref{target->index}
 		}
 	});
 }
 void mir_builder::build_cond_branch(mir_operand condition, mir_block* true_block, mir_block* false_block) {
+	s_assert(operand_type(condition).ty->is_bool(),
+		"Condition operand for conditional branch must be of boolean type");
+
+	s_assert(true_block != nullptr && false_block != nullptr,
+		"Both destination blocks cannot be null in conditional branch");
+
 	insert_instr({
 		.kind = mir_instr_opcode::BRA_CND,
 		.operands = {
 			condition,
-			mir_block_ref{true_block},
-			mir_block_ref{false_block}
+			mir_block_ref{true_block->index},
+			mir_block_ref{false_block->index}
 		}
 	});
 }
 
 mir_operand mir_builder::build_call(const mir_function* func, std::vector<mir_operand> args, const std::string& result_name) {
+	s_assert(func != nullptr, "Function reference cannot be null in call instruction");
+
 	// insert function as first operand
 	args.insert(args.begin(), mir_func_ref{ func });
 
@@ -131,15 +167,15 @@ mir_value mir_builder::build_cast(mir_operand value, mir_type target_type, const
 }
 
 void mir_builder::insert_instr(const mir_instr&& instr) {
-	if (!ins_block) {
-		throw std::runtime_error("Cannot insert an instruction when insert block is null");
-	}
+	s_assert(ins_block != nullptr,
+		"Cannot insert an instruction when insert block is null");
+
 	ins_block->push_instr(instr);
 }
 mir_value mir_builder::create_ssa_value(mir_type type, const std::string& name) {
-	if (!func) {
-		throw std::runtime_error("Cannot create a temporary value when function is null");
-	}
+	s_assert(func != nullptr,
+		"Cannot create SSA value when function is null");
+
 	return func->make_value(type);
 }
 
